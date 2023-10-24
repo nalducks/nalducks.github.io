039bc736d1bc0efd5790af7a82bd8d48da8a143e
Dhany Catur Prasetya <147501427+nalducks@users.noreply.github.com>
2023-10-24 17:59:17 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,82 +1,3 @@
-// script.js
-const navbar = document.querySelector(".navbar");
-const menuItems = navbar.querySelectorAll(".menu a");
-const sections = document.querySelectorAll("section");
-let isNavbarHidden = false;
-let timeoutId;
-
-// Fungsi untuk memunculkan navbar dengan efek transisi dari atas ke bawah
-function showNavbar() {
-    if (isNavbarHidden) {
-        navbar.style.transform = "translateY(0)";
-        isNavbarHidden = false;
-    }
-    setActiveMenuItem();
-}
-
-// Fungsi untuk menentukan bagian yang sedang terlihat
-function getCurrentSection() {
-    let currentSection = null;
-    sections.forEach((section) => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top <=100 && rect.bottom >= 100) {
-            currentSection = section;
-        }
-    });
-    return currentSection;
-}
-
-// Fungsi untuk menandai item menu yang aktif
-function setActiveMenuItem() {
-    const currentSection = getCurrentSection();
-    menuItems.forEach((menuItem) => {
-        const sectionId = menuItem.getAttribute("href").substring(1);
-        if (currentSection && currentSection.id === sectionId) {
-            menuItem.classList.add("active");
-        } else {
-            menuItem.classList.remove("active");
-        }
-    });
-}
-
-window.addEventListener("scroll", showNavbar);
-window.addEventListener("load", () => {
-    // Menambahkan efek transisi navbar dari atas ke bawah saat halaman dimuat pertama kali
-    navbar.style.transition = "transform 2s";
-    navbar.style.transform = "translateY(0)";
-});
-
-
-
-
-// Fungsi untuk slider img
-let currentIndex = 0;
-const imageContainers = document.querySelectorAll('.imageCertificate');
-
-function slide(direction) {
-  imageContainers[currentIndex].classList.remove('visible');
-  
-  if (direction === 'left') {
-    currentIndex = (currentIndex - 1 + imageContainers.length) % imageContainers.length;
-  } else if (direction === 'right') {
-    currentIndex = (currentIndex + 1) % imageContainers.length;
-  }
-  imageContainers[currentIndex].classList.add('visible');
-}
-
-
-// Fungsi Button Dropdown
-function showDropdown() {
-    var dropdown = document.getElementById("dropdown");
-    dropdown.classList.add("show");
-    dropdown.classList.remove("close");
-}
-function closeDropdown() {
-    var dropdown = document.getElementById("dropdown");
-    dropdown.classList.add("close");
-}
-
-
 var textToType = "MAINTENANCE TECHNICHIAN & PROGRAMMING";
 var textElement = document.getElementById("text");
 var index = 0;
@@ -142,3 +63,26 @@ window.addEventListener("load", () => {
     }
     setTimeout(munculkanElemen1, 6000);
     setTimeout(munculkanElemen2, 7000);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var menuItems = document.querySelectorAll(".menu a");
+    var sections = document.querySelectorAll("section");
+    var scrollOffsets = [];
+
+    sections.forEach(function(section) {
+        scrollOffsets.push(section.offsetTop);
+    });
+
+    window.addEventListener("scroll", function() {
+        var scrollPosition = window.scrollY;
+
+        for (var i = 0; i < scrollOffsets.length; i++) {
+            if (scrollPosition >= scrollOffsets[i] && (i === scrollOffsets.length - 1 || scrollPosition < scrollOffsets[i + 1])) {
+                menuItems.forEach(function(item) {
+                    item.classList.remove("active");
+                });
+                menuItems[i].classList.add("active");
+            }
+        }
+    });
+});
