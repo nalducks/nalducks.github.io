b5e85e46d632b25a3b38342656bf7b795e17a012
Dhany Catur Prasetya <147501427+nalducks@users.noreply.github.com>
2023-10-24 17:53:19 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 // script.js
 const navbar = document.querySelector(".navbar");
 const menuItems = navbar.querySelectorAll(".menu a");
-const sections = document.querySelectorAll(".section");
+const sections = document.querySelectorAll("section");
 let isNavbarHidden = false;
 let timeoutId;
 
